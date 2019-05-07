86845a0aabe8c0f257256e6ef853904af5b657b5
Kevin Reynolds <tossrock@gmail.com>
2019-05-06 18:04:38 -0700

    Update to GPL, add copyright header to files

--- playlists.cpp
+++ playlists.cpp
@@ -1,3 +1,12 @@
+/*  Copyright (C) 2019, Lightsale, Inc.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #include "playlists.h"
 
 const uint16_t pl_white_images[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
